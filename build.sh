5b4cded3449c35fb545bf61aabf9aced2c4a7cde
Kyle Burton <kyle.burton@gmail.com>
2013-10-14 20:42:02 -0400

    working on README

---
+++ build.sh
@@ -0,0 +1,1 @@
+go install github.com/kyleburton/go-abtab/pkg/abtab && go install github.com/kyleburton/go-abtab/cmd/abtab
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 go get github.com/kyleburton/go-abtab/cmd/abtab
 
-for b in cat cut fillrates grep head sort tail view; do
+for b in cat cut grep head sort tail view; do
 
   cat >$GOPATH/bin/ab$b <<HERE
 #!/usr/bin/env bash
@@ -13,3 +13,14 @@ HERE
 
   chmod 755 $GOPATH/bin/ab$b
 done
+
+cat >$GOPATH/bin/abfillrates <<HERE
+#!/usr/bin/env bash
+
+set -eu
+ROOT="\$(dirname \$( cd "\$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
+
+"$GOPATH/bin/abtab" -task fill-rates "\$@"
+HERE
+
+chmod 755 $GOPATH/bin/abfillrates
