b845fc5615036750d069a8d5e938e3e23fe26e4f
Kyle Burton <kyle.burton@gmail.com>
2014-04-03 21:14:08 -0400

    docs

--- scripts/fixedtest.sh
+++ scripts/fixedtest.sh
@@ -11,4 +11,4 @@ else
   echo "NOT: IS TERM"
 fi
 ./abtab -task cat -i fixtures/fill-rate-test-1.input.tab -o "$FIXED_URL" | \
-  ./abtab -task cat -i "fixed:///dev/stdin?fields=F1:0-10,F2:11-20,F3:21-40,F4:41-60,F5:61-80&padding=~" | abtab -task view
+  ./abtab -task cat -i "fixed:///dev/stdin?fields=F1:0-10,F2:11-20,F3:21-40,F4:41-60,F5:61-80&padding=~" | abview
